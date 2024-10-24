44dac4efce4a01f35fcf6452dfd6ef15edc067c8
Han <inte098@naver.com>
2024-10-24 11:18:12 +0900

    섹션4 - 회원 서비스 개발 MemberService 작성 Optional의 경우 get()을 통해 데이터를 바로 뽑아왔는데 이 방식을 권장하진 않는다고 함 ifPresent 또는 orElseGet을 사용하여 가져오는 방식을 권장

---
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,43 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        // NOTE : Optional의 경우 get을 사용하여 바로 꺼내는 방식을 권장하진 않음
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원압니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findALl();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
